20f267ccf2d966471701d76be77729ebadc18bc8
hbhb1969 <37511683+hbhb1969@users.noreply.github.com>
2018-05-20 15:53:21 +0200

    Aufgeräumt

--- public/scripts/fuetterungen.js
+++ public/scripts/fuetterungen.js
@@ -4,99 +4,70 @@ window.onload = function() {
 
   // Datum vorausfüllen
   document.getElementById('datum').valueAsDate = new Date();
-
-  let tbodyLager = document.getElementById("tbodyLager");
-  // Tabelle wird in die Variable data ausgelesen
-  tbodyLager.onclick = function(e) {
-    e = e || window.event;
-    let data = [];
-    let target = e.srcElement || e.target;
-    while (target && target.nodeName !== "TR") {
-      target = target.parentNode;
-    }
-    if (target) {
-      let cells = target.getElementsByTagName("td");
-      for (let i = 0; i < cells.length; i++) {
-        data.push(cells[i].innerHTML);
-      }
-    }
-    let id = data[0];
-    let datum = '20' + data[1].split('.')[2] + '-' + data[1].split('.')[1] + '-' + data[1].split('.')[0];
-    let menge = data[2];
-    let einheit = data[3];
-    let lager = data[4];
-    let lagerId = '';
-
-    // Daten der angeklickten Tabellenzeile werden in das Formular geschrieben
-    document.getElementsByName('F_ID')[0].value = id;
-    document.getElementsByName('F_Datum')[0].value = datum;
-    document.getElementsByName('F_BruttoMenge')[0].value = menge;
-    let options = document.getElementsByTagName('option');
-    for (let i = 0; i < options.length; i++) {
-      if (options[i].innerHTML == lager) {
-        lagerId = options[i].value;
-      }
-    }
-
-    document.getElementsByName('Lager_L_ID')[0].value = lagerId;
-
-    document.getElementById('btn-buchen').classList.add('nodisplay');
-    document.getElementById('btn-aendern').classList.remove('nodisplay');
-    document.getElementById('btn-loeschen').classList.remove('nodisplay');
-    document.getElementById('btn-abbrechen').classList.remove('nodisplay');
-
-  };
-
-
-  let tbodyDirekt = document.getElementById("tbodyDirekt");
-  // Tabelle wird in die Variable data ausgelesen
-  tbodyDirekt.onclick = function(e) {
-    e = e || window.event;
-    let data = [];
-    let target = e.srcElement || e.target;
-    while (target && target.nodeName !== "TR") {
-      target = target.parentNode;
-    }
-    if (target) {
-      let cells = target.getElementsByTagName("td");
-      for (let i = 0; i < cells.length; i++) {
-        data.push(cells[i].innerHTML);
-      }
-    }
-    let id = data[0];
-    let datum = '20' + data[1].split('.')[2] + '-' + data[1].split('.')[1] + '-' + data[1].split('.')[0];
-    let menge = data[2];
-    let einheit = data[3];
-    let stoff = data[4];
-    let stoffId = '';
-
-    // Daten der angeklickten Tabellenzeile werden in das Formular geschrieben
-    document.getElementsByName('F_ID')[0].value = id;
-    document.getElementsByName('F_Datum')[0].value = datum;
-    document.getElementsByName('F_BruttoMenge')[0].value = menge;
-    let options = document.getElementsByTagName('option');
-    for (let i = 0; i < options.length; i++) {
-      if (options[i].innerHTML == stoff) {
-        stoffId = options[i].value;
-      }
-    }
-
-    document.getElementsByName('Stoff_S_ID')[0].value = stoffId;
-
-    document.getElementById('btn-buchen').classList.add('nodisplay');
-    document.getElementById('btn-aendern').classList.remove('nodisplay');
-    document.getElementById('btn-loeschen').classList.remove('nodisplay');
-    document.getElementById('btn-abbrechen').classList.remove('nodisplay');
-
-  }
 }
 
+// Bei Klick auf eine Tabellenzeile werden die Daten in das Formular übertragen
+function auswahlTabelle(e) {
+  e = e || window.event;
+  let data = [];
+  let target = e.srcElement || e.target;
+  while (target && target.nodeName !== "TR") {
+    target = target.parentNode;
+  }
+  if (target) {
+    let cells = target.getElementsByTagName("td");
+    for (let i = 0; i < cells.length; i++) {
+      data.push(cells[i].innerHTML);
+    }
+  }
+  let id = data[0];
+  let datum = '20' + data[1].split('.')[2] + '-' + data[1].split('.')[1] + '-' + data[1].split('.')[0];
+  let menge = data[2];
+  let einheit = data[3];
+  let lagerRadio = document.getElementById("lager-radio");
+  let lager = '';
+  let stoff = '';
+  if (lagerRadio.checked == true) {
+
+    lager = data[4];
+  } else {
+    stoff = data[4];
+  }
+  let lagerId = '';
+  let stoffId = '';
+
+  // Daten der angeklickten Tabellenzeile werden in das Formular geschrieben
+  document.getElementsByName('F_ID')[0].value = id;
+  document.getElementsByName('F_Datum')[0].value = datum;
+  document.getElementsByName('F_BruttoMenge')[0].value = menge;
+  let options = document.getElementsByTagName('option');
+  for (let i = 0; i < options.length; i++) {
+    if (options[i].innerHTML == lager) {
+      lagerId = options[i].value;
+      break;
+    }
+    if (options[i].innerHTML == stoff) {
+      stoffId = options[i].value;
+      break;
+    }
+  }
+
+  document.getElementsByName('Lager_L_ID')[0].value = lagerId;
+  document.getElementsByName('Stoff_S_ID')[0].value = stoffId;
+  document.getElementById('btn-buchen').classList.add('nodisplay');
+  document.getElementById('btn-aendern').classList.remove('nodisplay');
+  document.getElementById('btn-loeschen').classList.remove('nodisplay');
+  document.getElementById('btn-abbrechen').classList.remove('nodisplay');
+}
+
+// Aktualisiert die Mengeneinheit je nach Auswahl in den Selects
 function meAktualisieren(selectName) {
   let selectbox = document.getElementById(selectName);
   let mebez = selectbox.options[selectbox.selectedIndex].getAttribute('mebez');
   document.getElementById('fuetterung-me').innerHTML = '(' + mebez + ')';
 }
 
+// Wechselt zwischen Lager- und Direktrohstoff
 function ansichtWechseln() {
   let lagerRadio = document.getElementById("lager-radio");
   if (lagerRadio.checked == true) {
